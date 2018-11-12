15221c689a736f34344c0675c84004c4b016fced
Zhao Jun <root@zhaojun.im>
2018-11-12 23:28:39 +0800

    :zap: 更新系统权限信息时, 动态刷新 Shiro 过滤器链, 采用注解 + AOP 方式.

---
+++ src/main/java/im/zhaojun/aop/updateFilterChainAspect.java
@@ -0,0 +1,30 @@
+package im.zhaojun.aop;
+
+import im.zhaojun.service.ShiroService;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Aspect
+@Component
+public class updateFilterChainAspect {
+
+    @Resource
+    private ShiroService shiroService;
+
+    private static final Logger logger = LoggerFactory.getLogger(updateFilterChainAspect.class);
+
+    @Pointcut("@annotation(im.zhaojun.annotaion.UpdateFilterChain)")
+    public void updateFilterChain() {}
+
+    @After("updateFilterChain()")
+    public void doAfter() {
+        shiroService.updateFilterChain();
+    }
+
+}
