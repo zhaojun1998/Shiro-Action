acf6bf77186506ba360ab2c9679514fb6750bb0e
zhaojun <root@zhaojun.im>
2018-12-22 23:30:15 +0800

    :sparkles: 操作日志功能.

---
+++ src/main/java/im/zhaojun/mapper/SysLogMapper.java
@@ -0,0 +1,23 @@
+package im.zhaojun.mapper;
+
+import im.zhaojun.model.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysLog record);
+
+    int insertSelective(SysLog record);
+
+    SysLog selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysLog record);
+
+    int updateByPrimaryKey(SysLog record);
+
+    List<SysLog> selectAll();
+}
