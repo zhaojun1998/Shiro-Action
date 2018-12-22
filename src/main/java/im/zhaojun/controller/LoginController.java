5c88fe616a447af8e588aa57400425b5166f5a72
zhaojun <root@zhaojun.im>
2018-12-22 23:25:06 +0800

    :zap: 修改验证码生成方式, 去除易混淆的字符.

--- src/main/java/im/zhaojun/controller/LoginController.java
+++ src/main/java/im/zhaojun/controller/LoginController.java
@@ -1,14 +1,13 @@
 package im.zhaojun.controller;
 
-import cn.hutool.captcha.CaptchaUtil;
-import cn.hutool.captcha.CircleCaptcha;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.extra.mail.MailUtil;
 import im.zhaojun.exception.CaptchaIncorrectException;
 import im.zhaojun.exception.UserAlreadyExistsException;
 import im.zhaojun.model.User;
 import im.zhaojun.service.UserService;
 import im.zhaojun.util.ResultBean;
+import im.zhaojun.util.code.Captcha;
+import im.zhaojun.util.code.GifCaptcha;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.session.Session;
@@ -20,16 +19,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @Controller
 public class LoginController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private MailService mailService;
 
     @GetMapping("login")
     public String login() {
@@ -46,7 +46,9 @@ public class LoginController {
     public ResultBean<Boolean> login(User user, String captcha) {
         Subject subject = SecurityUtils.getSubject();
         String realCaptcha = (String) SecurityUtils.getSubject().getSession().getAttribute("captcha");
-        if (!realCaptcha.equals(captcha)) {
+
+        // session 中的验证码过期了
+        if (realCaptcha == null || realCaptcha.equals(captcha.toLowerCase()) == false) {
             throw new CaptchaIncorrectException();
         }
         UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword());
@@ -78,7 +80,7 @@ public class LoginController {
         String mailContent = "感谢您注册 Shiro-Action, 请验证您的邮箱，链接为： http://localhost/active?token=" + activeCode;
 
         new Thread(() ->
-                MailUtil.send(user.getEmail(), "Shiro-Action 激活", mailContent, false))
+                mailService.sendSimpleMail(user.getEmail(), "Shiro-Action 激活邮件", mailContent))
                 .start();
 
         // 注册后默认的角色, 根据自己数据库的角色表 ID 设置
@@ -88,15 +90,17 @@ public class LoginController {
 
     @GetMapping("captcha")
     public void captcha(HttpServletResponse response) throws IOException {
-        //定义图形验证码的长、宽、验证码字符数、干扰元素个数
-        CircleCaptcha shearCaptcha = CaptchaUtil.createCircleCaptcha(160, 38, 4, 0);
+        Captcha captcha = new GifCaptcha(160, 38, 4);
+
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpg");
+
+        captcha.out(response.getOutputStream());
 
         Session session = SecurityUtils.getSubject().getSession();
-        session.setAttribute("captcha", shearCaptcha.getCode());
-
-        response.setContentType("image/png");
-        OutputStream os = response.getOutputStream();
-        ImageIO.write(shearCaptcha.getImage(), "png", os);
+        session.setAttribute("captcha", captcha.text().toLowerCase());
     }
 
     @GetMapping("active")
