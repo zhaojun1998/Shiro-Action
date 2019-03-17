409a69aceb9182d15037ce1f14595fc5882f8f0b
zhaojun <root@zhaojun.im>
2019-03-17 21:11:12 +0800

    :bug: 修复判断用户是否存在的 bug

--- src/main/java/im/zhaojun/service/UserService.java
+++ src/main/java/im/zhaojun/service/UserService.java
@@ -94,7 +94,7 @@ public class UserService {
     }
 
     public boolean checkUserNameExist(String username) {
-        return userMapper.countByUserName(username) == 1;
+        return userMapper.countByUserName(username) > 1;
     }
 
     public void offlineBySessionId(String sessionId) {
