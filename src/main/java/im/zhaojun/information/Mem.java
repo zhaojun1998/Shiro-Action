bb4fe5a40e731a982ce722859bc1ce651ed58469
zhaojun <root@zhaojun.im>
2019-04-08 22:40:42 +0800

    :sparkles: 新增系统监控功能

---
+++ src/main/java/im/zhaojun/information/Mem.java
@@ -0,0 +1,53 @@
+package im.zhaojun.information;
+
+
+/**
+ * 內存相关信息
+ *
+ * @author YI
+ * @date 2019年1月22日15:11:56
+ */
+public class Mem {
+    /**
+     * 内存总量
+     */
+    private double total;
+
+    /**
+     * 已用内存
+     */
+    private double used;
+
+    /**
+     * 剩余内存
+     */
+    private double free;
+
+    public double getTotal() {
+        return Arith.div(total, (1024 * 1024 * 1024), 2);
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public double getUsed() {
+        return Arith.div(used, (1024 * 1024 * 1024), 2);
+    }
+
+    public void setUsed(long used) {
+        this.used = used;
+    }
+
+    public double getFree() {
+        return Arith.div(free, (1024 * 1024 * 1024), 2);
+    }
+
+    public void setFree(long free) {
+        this.free = free;
+    }
+
+    public double getUsage() {
+        return Arith.mul(Arith.div(used, total, 4), 100);
+    }
+}
